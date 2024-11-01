eae907304e9dc691094d37950dabf46980254792
DaniHP <DaniHP@>
2024-11-01 21:28:28 +0100

    commit CodiceEsempio com2

--- src/it/dv/CodiceEsempio.java
+++ src/it/dv/CodiceEsempio.java
@@ -7,20 +7,35 @@ public class CodiceEsempio {
 
 	public static void main(String[] args) {
 		List<Integer> lista = new ArrayList<Integer>();
-		lista.add(12);
+		lista.add(12);//aggiuno alla lista il numero 12 nella posizione 0
 		System.out.println("la prima posizione e'  il numero " +lista.get(0));
 		lista.add(89);
-		lista.add(0, 55);
-		System.out.println("la prima posizione e'  il numero " +lista.get(0));
+		lista.add(0, 55);//aggiungo alla lista in posizione 0 il numero 55, gli altri scalano
+		System.out.println("la prima posizione e' ora il numero " +lista.get(0));
 		
 		for ( int i=0 ; i<lista.size(); i++)
-			System.out.println( "numero " + lista.get(i));
+			System.out.println( "numero alla posizione " + i +" = " + lista.get(i));
 		
-		List<String> listaPersona = new ArrayList();
-
+		//altro modo di stampare il contenuto della lista
+		for(Integer intero: lista) //oppure int
+			System.out.println("il numero è: " + intero);
+		//con arrays
+		String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+		for (String i : cars) {
+		  System.out.println(i);
+		}
+		
+		
+		//Posso fare altre lista di altre cose o liste di altre classi
+		List<String> listaNomi = new ArrayList<String>();
+		
+		
 			
 		
 		
 	}
+		
 
 }
+
+
